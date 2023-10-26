5810f63cb66eab96ed279f6fc6f50027224879de
[Nswilde] <142635621+nswilde@users.noreply.github.com>
2023-10-26 14:36:44 +1300

    list now shows multiple items in array

---
+++ RememberMe/ContentViewExtension.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewExtension.swift
+//  RememberMe
+//
+//  Created by Nikki Wilde on 24/10/23.
+//
+
+import SwiftUI
+
+struct ContentViewExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewExtension()
+}

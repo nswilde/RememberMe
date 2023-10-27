0d7639fd39dfe4a9da8a2673f6bd7e7142207caf
[Nswilde] <142635621+nswilde@users.noreply.github.com>
2023-10-27 13:25:15 +1300

    finished 77 challenge

---
+++ RememberMe/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  RememberMe
+//
+//  Created by Nikki Wilde on 27/10/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
